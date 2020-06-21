1b7ca577065c738e43a57723d4b0586a3900171c
edward_chen <872990547@qq.com>
2020-06-21 16:22:27 +0800

    提交

--- express/controller/user.js
+++ express/controller/user.js
@@ -9,7 +9,7 @@ const {
 const login = (username, passsword) => {
     username = escape(username);
     passsword = escape(genPassword(passsword));
-    const sql = `select username,realname from users where username=${username}and password=${passsword}`;
+    const sql = `select username,realname from users where username=${username} and password=${passsword}`;
     return exec(sql).then(rows => {
         return rows[0] || {};
     });
--- koa2/controller/user.js
+++ koa2/controller/user.js
@@ -9,7 +9,7 @@ const {
 const login = async (username, passsword) => {
     username = escape(username);
     passsword = escape(genPassword(passsword));
-    const sql = `select username,realname from users where username=${username}and password=${passsword}`;
+    const sql = `select username,realname from users where username=${username} and password=${passsword}`;
     const rows = await exec(sql);
     return rows[0] || {};
 }
